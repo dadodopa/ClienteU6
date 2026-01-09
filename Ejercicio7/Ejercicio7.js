2ae703378412bd40eb78fd2bd965e26e0a0c8a90
dadodopa <dopazodominguez@gmail.com>
2026-01-09 13:18:33 +0100

    09/01/2026

--- Ejercicio7/Ejercicio7.js
+++ Ejercicio7/Ejercicio7.js
@@ -1,21 +1,34 @@
 let edificios=[]
-function crearEdificio(direccion, plantas, puertas) {
-    edificios.forEach(edificio => {
-        if(edificio.esIgual(direccion)){
-            return
-        }
-    });
-}
-class Edificio {
 
-    constructor(direccion, plantas, puertas) {
-        this.direccion=direccion
-        this.plantas=plantas
-        this.puertas=puertas
+document.getElementById("crearEdificio").addEventListener("click", function () {
+    
+    const direccion = document.getElementById("direccionEdificio").value
+    const plantas = document.getElementById("plantasEdificio").value
+    const puertas = document.getElementById("puertasEdificio").value
+
+    //Creación de las plantas
+    //Primero creamos el array donde estan las plantas
+    const plantasArray=[]
+    //Creamos el número de plantas que nos de el usuario con un bucle
+    for (let index = 0; index < plantas; index++) {
+        //Añadimos un array de puertas a cada planta
+        const puertasArray=[]
+        for (let j = 0; j < puertas; j++) {
+            //Añadimos a cada puerta que está vacia
+            puertasArray.push("Vacío")
+        }
+        //Añadimos ese array de puertas a la planta y se repite tanto como numero de plantas que hay
+        plantasArray.push(puertasArray)
     }
-    esIgual(direccion){
-        if(this.direccion==direccion){
-            return true
-        } else false
+
+    let edificio=new Edificio(direccion, plantasArray)
+    edificios.push(edificio)
+    console.log(edificios)
+})
+
+class Edificio {
+    constructor(direccion) {
+        this.direccion=direccion;
+        this.plantas=[];
     }
 }
